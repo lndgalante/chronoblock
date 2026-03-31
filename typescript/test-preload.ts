f151b4793461082e347a37287d791cd5b3f36150
Leonardo Galante <lndgalante@gmail.com>
2026-03-31 18:17:18 -0300

    Remove completely TypeScript implementation

--- typescript/test-preload.ts
+++
@@ -1,4 +0,0 @@
-process.env.ETH_RPC_URL = "http://test-eth";
-process.env.SCROLL_RPC_URL = "http://test-scroll";
-process.env.INK_RPC_URL = "http://test-ink";
-process.env.HYPEREVM_RPC_URL = "http://test-hyperevm";
